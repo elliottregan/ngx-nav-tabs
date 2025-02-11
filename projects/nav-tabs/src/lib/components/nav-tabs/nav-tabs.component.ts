50a9ceb4c5ede96740854ff7a4a841ffc7b52dab
Elliott Regan <950202+elliottregan@users.noreply.github.com>
2025-02-11 13:22:13 -0700

    move redirects

--- projects/nav-tabs/src/lib/components/nav-tabs/nav-tabs.component.ts
+++ projects/nav-tabs/src/lib/components/nav-tabs/nav-tabs.component.ts
@@ -53,9 +53,7 @@ export class NavTabsComponent<TKeys extends string = string> implements OnChange
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['routes']) {
-      console.log(this.sortedRoutes);
       this.sortRoutes();
-      console.log(this.sortedRoutes);
     }
   }
 
