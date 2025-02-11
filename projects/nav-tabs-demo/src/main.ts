2c00c3b1115be88e87ac4d298dbecb92b179ca6c
Elliott Regan <950202+elliottregan@users.noreply.github.com>
2025-02-11 13:32:29 -0700

    make it zoneless

--- projects/nav-tabs-demo/src/main.ts
+++ projects/nav-tabs-demo/src/main.ts
@@ -2,7 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import {baseRoute, routes} from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import {provideExperimentalZonelessChangeDetection} from "@angular/core";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter([...baseRoute, ...routes])],
+  providers: [
+    provideRouter([...baseRoute, ...routes]),
+    provideExperimentalZonelessChangeDetection(),
+  ],
 });
